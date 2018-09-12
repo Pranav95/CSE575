7ed350d4b20508a9e2cf5f473cd4f7e4750f24d3
Pranav Havanurkar <pgodse95@gmail.com>
2018-09-12 10:22:38 -0700

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -2,6 +2,8 @@ from sklearn.feature_extraction import DictVectorizer
 from collections import Counter, OrderedDict
 import os
 import re
+import numpy as np
+
 
 v = DictVectorizer()
 stopwords = list()
@@ -11,6 +13,8 @@ with open('stopwords.txt', 'r') as f:
 		stopwords.append(line.strip())
 
 
+
+count = 0
 document = list()
 
 path1 = "/home/pranav/Statistical Machine Learning/movie_review_data/neg"
@@ -24,11 +28,21 @@ for filename in os.listdir(path1):
 		for word in line:
 			if word not in stopwords:
 				finalist.append(word)
-	document.append(finalist)
+	document.append(" ".join(finalist))
+	count = count + 1
+	if count>200:
+		break
 
 
-v = DictVectorizer()
+'''v = DictVectorizer()
 
 X = v.fit_transform(Counter(f) for f in document)
+v = X.toarray()
+v = v.transpose()
 
-print(X.A)
+
+for b in np.nditer(v):
+	print(b)'''
+
+bagsofwords = [Counter(re.findall(r'\w+', txt)) for txt in document]
+print(bagsofwords[0])
